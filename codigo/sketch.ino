96b06eae17522c39c52c41e51cdf62faa9abcb7b
Giovana Palhares <61204787+GiovanaPalhares@users.noreply.github.com>
2026-05-01 14:31:43 -0300

    Update and rename sketch.ino to codigo/sketch.ino

--- codigo/sketch.ino
+++ codigo/sketch.ino
@@ -1,19 +1,19 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-// WiFi
+// WiFi (Wokwi)
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-// MQTT
-const char* mqtt_server = "test.mosquitto.org";
+// MQTT Broker público
+const char* mqtt_server = "broker.hivemq.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Pinos
 const int ldrPin = 34;
-const int ledPin = 2;
+const int ledPin = 4;
 
 // Variáveis
 int valorLuz = 0;
@@ -21,25 +21,34 @@ int limite = 2000;
 
 void setup() {
   Serial.begin(115200);
-
   pinMode(ledPin, OUTPUT);
 
+  // WiFi
   WiFi.begin(ssid, password);
+  Serial.print("Conectando ao WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("\nWiFi conectado!");
 
-  Serial.println("\nWiFi conectado");
-
+  // MQTT
   client.setServer(mqtt_server, 1883);
 }
 
 void reconnect() {
   while (!client.connected()) {
-    if (client.connect("esp32Client")) {
-      Serial.println("MQTT conectado!");
+    Serial.print("Conectando ao MQTT...");
+
+    String clientId = "esp32Client_";
+    clientId += String(random(1000));
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Conectado!");
     } else {
+      Serial.print("Erro, rc=");
+      Serial.print(client.state());
+      Serial.println(" tentando novamente...");
       delay(2000);
     }
   }
@@ -52,12 +61,16 @@ void loop() {
 
   client.loop();
 
+  // Leitura do sensor
   valorLuz = analogRead(ldrPin);
 
   Serial.print("Luminosidade: ");
   Serial.println(valorLuz);
 
-  if (valorLuz < limite) {
+  // Lógica (invertida por causa do módulo)
+  bool escuro = valorLuz > limite;
+
+  if (escuro) {
     digitalWrite(ledPin, HIGH);
     client.publish("iluminacao/status", "Ligado");
   } else {
@@ -65,7 +78,8 @@ void loop() {
     client.publish("iluminacao/status", "Desligado");
   }
 
-  char msg[10];
+  // Enviar valor da luminosidade
+  char msg[20];
   sprintf(msg, "%d", valorLuz);
   client.publish("iluminacao/luminosidade", msg);
 
